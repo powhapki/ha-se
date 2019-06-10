eeff5ff252437fb857e139d2f2ad068acbc172b3
powhapki <powhapki@gmail.com>
2019-06-10 23:23:07 +0900

    Update config_consul_server.sh

--- config_consul_server.sh
+++ config_consul_server.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-
 sudo mkdir --parents /etc/consul.d
 sudo touch /etc/consul.d/consul.hcl
+local_ip=`echo $(hostname -I)|tr -d ''`
 
 sudo cat <<EOF > /etc/consul.d/consul.hcl
+server ="true"
+node_name ="consul_$local_ip"
 datacenter = "dc1"
 data_dir = "/opt/consul"
+bind_addr ="0.0.0.0"
+client_addr ="0.0.0.0"
+advertise_addr = "$local_ip"
+bootstrap_expect = 3
+retry_join = ["172.31.60.101", "172.31.60.102", "172.31.60.103"]
+ui = "true"
+log_level = "DEBUG"
+enable_syslog = "true"
+acl_enforce_version_8 = "false"
 encrypt="$(consul keygen)"
-retry_join = ["172.31.0.2"]
 EOF
 
 sudo chown --recursive consul:consul /etc/consul.d

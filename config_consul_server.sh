d906db6badb303287819ea3328ef36bc504241fc
powhapki <powhapki@gmail.com>
2019-06-10 22:56:07 +0900

    Update config_consul_server.sh

--- config_consul_server.sh
+++ config_consul_server.sh
@@ -8,6 +8,7 @@ sudo cat <<EOF > /etc/consul.d/consul.hcl
 datacenter = "dc1"
 data_dir = "/opt/consul"
 encrypt="$(consul keygen)"
+retry_join = ["172.31.0.2"]
 EOF
 
 sudo chown --recursive consul:consul /etc/consul.d

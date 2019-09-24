3d0243a0e9fe830ccc0d5b15df8317ef464c43bf
powhapki <powhapki@gmail.com>
2019-09-24 23:53:34 +0900

    need to define file storage

--- terraform-vault-cluster/config_vault_server.sh
+++ terraform-vault-cluster/config_vault_server.sh
@@ -2,9 +2,9 @@
 
 local_ip=`echo $(hostname -I)|tr -d ''`
 sudo mkdir --parents /etc/vault.d
-sudo touch /etc/vault.d/vault.hcl
+sudo touch /etc/vault.d/vault_server.hcl
 sudo chown --recursive vault:vault /etc/vault.d
-sudo chmod 640 /etc/vault.d/vault.hcl
+sudo chmod 640 /etc/vault.d/vault_server.hcl
 
 sudo cat <<EOF > /etc/vault.d/vault_server.hcl
 ui = true
@@ -13,6 +13,10 @@ listener "tcp" {
   cluster_address  = "$local_ip:8201"
   tls_disable      = "true"
 }
+storage "file" {
+  path = "/mnt/vault/data"
+}
+
 api_addr = "http://$local_ip:8200"
 cluster_addr = "https://$local_ip:8201"
 EOF

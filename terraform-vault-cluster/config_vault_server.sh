45101c2e4631976635520c02d4983fbbe0079cbd
powhapki <powhapki@gmail.com>
2019-09-24 21:27:26 +0900

    Modified 4 SEC IM PoC

--- terraform-vault-cluster/config_vault_server.sh
+++ terraform-vault-cluster/config_vault_server.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 local_ip=`echo $(hostname -I)|tr -d ''`
-sudo mkdir --parents /usr/local/etc/vault/
-sudo touch /usr/local/etc/vault/vault_server.hcl
+sudo mkdir --parents /etc/vault.d
+sudo touch /etc/vault.d/vault.hcl
+sudo chown --recursive vault:vault /etc/vault.d
+sudo chmod 640 /etc/vault.d/vault.hcl
 
-
-sudo cat <<EOF > /usr/local/etc/vault/vault_server.hcl
+sudo cat <<EOF > /etc/vault.d/vault_server.hcl
 ui = true
 listener "tcp" {
   address          = "0.0.0.0:8200"
   cluster_address  = "$local_ip:8201"
   tls_disable      = "true"
 }
-
 api_addr = "http://$local_ip:8200"
 cluster_addr = "https://$local_ip:8201"
-
 EOF
-
-sudo chown --recursive vault:vault /var/run/vault
-sudo chmod 640 /usr/local/etc/vault/vault_server.hcl

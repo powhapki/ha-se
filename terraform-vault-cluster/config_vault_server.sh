6941a96654a71dc1e1a9f5ed5877537a1e021c97
powhapki <powhapki@gmail.com>
2019-09-24 23:56:45 +0900

    need to create file storage folder

--- terraform-vault-cluster/config_vault_server.sh
+++ terraform-vault-cluster/config_vault_server.sh
@@ -5,6 +5,9 @@ sudo mkdir --parents /etc/vault.d
 sudo touch /etc/vault.d/vault_server.hcl
 sudo chown --recursive vault:vault /etc/vault.d
 sudo chmod 640 /etc/vault.d/vault_server.hcl
+sudo mkdir --parents /mnt/vault
+sudo touch /mnt/vault/data
+sudo chown --recursive vault:vault /mnt/vault
 
 sudo cat <<EOF > /etc/vault.d/vault_server.hcl
 ui = true
